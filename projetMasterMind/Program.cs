ce5565180c7cb49bd60cfaa0b0de45230e44b25e
Maxime Pelloquin <pt11pfm@eduvaud.ch>
2023-09-08 15:47:55 +0200

    Version 08.09.2023

--- projetMasterMind/Program.cs
+++ projetMasterMind/Program.cs
@@ -9,11 +9,21 @@ namespace projetMasterMind
     internal class Program
     {
         static void Main(string[] args)
+            //Création des valeurs pour stocker les valeur
+            string red;
+            string green;
+            string blue;
+            string yellow;
+            string magenta;
+            string white;
+            string cyan;
+
         {   //Message de Bienvenue sur la console, avec les explications du jeux.
             Console.WriteLine("Bienvenue sur Mastermind !");
             Console.WriteLine("Couleur possible : GYWRMC");
             Console.WriteLine("Devine le code en 4 couleur.");
             Console.ReadLine();
         }
+
     }
 }
