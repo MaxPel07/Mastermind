9cb775805a935ea171255fc0647adb17704a0c0c
Maxime Pelloquin <pt11pfm@eduvaud.ch>
2023-09-15 15:40:58 +0200

    Version 15.09.2023

--- projetMasterMind/Program.cs
+++ projetMasterMind/Program.cs
@@ -8,8 +8,8 @@ namespace projetMasterMind
 {
     internal class Program
     {
-        static void Main(string[] args)
-            //Création des valeurs pour stocker les valeur
+        static void Main(string[] args) {
+            //Création des valeurs pour stocker les variables
             string red;
             string green;
             string blue;
@@ -17,11 +17,28 @@ namespace projetMasterMind
             string magenta;
             string white;
             string cyan;
+            string essaieutilisateur;
+            string combinaisonJuste = "GGGG";
 
-        {   //Message de Bienvenue sur la console, avec les explications du jeux.
+           //Message de Bienvenue sur la console, avec les explications du jeux.
             Console.WriteLine("Bienvenue sur Mastermind !");
             Console.WriteLine("Couleur possible : GYWRMC");
-            Console.WriteLine("Devine le code en 4 couleur.");
+            Console.WriteLine("Devine le code en 4 couleur.\n");
+            for (int nbEssaie = 1; nbEssaie < 11; nbEssaie++)
+            {
+                Console.WriteLine("Essaie" + nbEssaie);
+                string essaieUtilisateur=Console.ReadLine();
+                if (essaieUtilisateur == combinaisonJuste)
+                {
+                    Console.WriteLine("Bravo ! Vous avez trouvé la bonne combinaison");
+                }
+                else if (essaieUtilisateur != combinaisonJuste)
+                {
+                    Console.WriteLine("mauvaise réponse");
+                }
+
+            }
+
             Console.ReadLine();
         }
 
