1590534856d5e120f910d2a76bce28634ef3dc20
Maxime Pelloquin <pt11pfm@eduvaud.ch>
2023-09-29 15:48:57 +0200

    Il manque une partie de code permettant de vérifier les couleurs "ok"
    Signed-off-by: Maxime Pelloquin <pt11pfm@eduvaud.ch>

--- projetMasterMind/Program.cs
+++ projetMasterMind/Program.cs
@@ -1,6 +1,6 @@
 ﻿///ETML
-///Auteur : Maxime Pelloquin
-///Date   : 22.09.2023
+///Auteur :      Maxime Pelloquin
+///Date   :      22.09.2023
 ///Description : Ce programme est un jeu communément appelé MasterMind. L'ordinateur choisit aléatoirement une suite de 4 couleurs
 ///              et le joueur doit trouvé cette combinaison secrète en essayant plusieur combinaison. Le grogramme lui indique si
 ///              dans la combinaison qu'il a inséré il y a des couleurs de bien placé et des bonnes couleurs mais en mauvaise position.
@@ -32,10 +32,10 @@ namespace projetMasterMind
                 Console.WriteLine("Devine le code en 4 couleur.\n");
 
                 Random random = new Random();
-                int color1= random.Next();
-                int color2= random.Next();
-                int color3= random.Next();
-                int color4= random.Next();
+                int color1 = random.Next(6);
+                int color2 = random.Next(6);
+                int color3 = random.Next(6);
+                int color4 = random.Next(6);
 
                 switch (color1)
                 {
@@ -162,20 +162,59 @@ namespace projetMasterMind
                 }
 
                 //10 essaie possible avec comme réponse système "mauvaise réponse" ou "Bravo..." pour le moment
-
+                //goal.char
                 for (int nbEssaie = 1; nbEssaie < 11; nbEssaie++)
                 {
                     Console.WriteLine("Essaie" + nbEssaie);
-                    string tryUser = Console.ReadLine();
-                    if (tryUser == goal)
+                    tryUser = Console.ReadLine();
+
+                    /*
+                    int ok = 0;
+                    for (int i = 0; i < goal.Length; i++)
                     {
-                        Console.WriteLine("Bravo ! Vous avez trouvé la bonne combinaison");
-                        break; //sort de la boucle si la combinaison est correcte
+                        char goalChar = goal[i];
+                        char tryUserChar = tryUser[i];
+
+                        if((tryUser[i] == goal[i]))
+                         
+                        {
+                            ok++;
+                        }
                     }
-                    else if (tryUser != goal)
+                    */
+                    int ok = 0;
+                    
+                        if (tryUser == goal.ToString())
+                        {
+                            Console.WriteLine("Bravo ! Vous avez trouvé la bonne combinaison");
+                            break; //sort de la boucle si la combinaison est correcte
+                        }
+                        else 
+                        {
+                            if (tryUser[0] == goal[0])
+                            {
+                                ok = ok + 1;
+                            }
+                            if (tryUser[1] == goal[1])
+                            {
+                                ok = ok + 1;
+                            }
+                            if (tryUser[2] == goal[2])
+                            {
+                                ok = ok + 1;
+                            }
+                            if (tryUser[3] == goal[3])
+                            {
+                                ok = ok + 1;
+                            }
+                            Console.WriteLine("Bravo ! Vous avez trouvé"+ok+" bonnes couleurs la bonne combinaison");
+                            break; //sort de la boucle si la combinaison est correcte
+                        }
+                    /*
+                    else if (tryUser != goal.ToString())
                     {
                         Console.WriteLine("mauvaise réponse");
-                    }
+                    }*/
 
                 }
 
